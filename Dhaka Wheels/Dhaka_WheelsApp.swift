0266b485d9f1493d770271eb753f2c21129f4aed
Md. Asadul Islam <asadul_islam@outlook.com>
2023-10-12 21:06:24 +0600

    Initial Commit

---
+++ Dhaka Wheels/Dhaka_WheelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dhaka_WheelsApp.swift
+//  Dhaka Wheels
+//
+//  Created by Md. Asadul Islam on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Dhaka_WheelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
