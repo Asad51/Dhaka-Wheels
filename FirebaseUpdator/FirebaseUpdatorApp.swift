2231a016a486bfc69bbc53d28d1cb45da3b69b02
Md. Asadul Islam <asadul_islam@outlook.com>
2023-10-15 01:31:14 +0600

    Added new app to update firebase data
    Signed-off-by: Md. Asadul Islam <asadul_islam@outlook.com>

---
+++ FirebaseUpdator/FirebaseUpdatorApp.swift
@@ -0,0 +1,30 @@
+//
+//  FirebaseUpdatorApp.swift
+//  FirebaseUpdator
+//
+//  Created by Md. Asadul Islam on 15/10/23.
+//
+
+import FirebaseCore
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
+
+@main
+struct FirebaseUpdatorApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
