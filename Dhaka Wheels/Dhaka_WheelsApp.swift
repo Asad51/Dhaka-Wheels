aca5fc0a363a64f576f52c71076305465a7f8e03
Md. Asadul Islam <asadul_islam@outlook.com>
2023-10-15 01:05:06 +0600

    Added Firebase to the project
    Signed-off-by: Md. Asadul Islam <asadul_islam@outlook.com>

--- Dhaka Wheels/Dhaka_WheelsApp.swift
+++ Dhaka Wheels/Dhaka_WheelsApp.swift
@@ -5,10 +5,23 @@
 //  Created by Md. Asadul Islam on 12/10/23.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct Dhaka_WheelsApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
