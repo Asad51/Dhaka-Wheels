e3ae6ad443d4a9399f447eaf1335475e4e79a821
Md. Asadul Islam <asadul_islam@outlook.com>
2023-10-29 01:18:55 +0600

    Implement bus search

---
+++ Dhaka Wheels/Extensions/Color+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extensions.swift
+//  Dhaka Wheels
+//
+//  Created by Md. Asadul Islam on 29/10/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static var teal: Color {
+        return self.init(uiColor: .systemTeal)
+    }
+}
--- Dhaka Wheels/Views/Bus/SearchView.swift
+++ Dhaka Wheels/Views/Bus/SearchView.swift
@@ -1,108 +1,178 @@
-//
-//  SearchView.swift
-//  Dhaka Wheels
-//
-//  Created by Md. Asadul Islam on 12/10/23.
-//
+    //
+    //  SearchView.swift
+    //  Dhaka Wheels
+    //
+    //  Created by Md. Asadul Islam on 12/10/23.
+    //
 
 import SwiftUI
 
 struct SearchView: View {
     @EnvironmentObject private var firebaseData: FirebaseData
-    
+
     @State private var startingLocation: String = ""
     @State private var endingLocation: String = ""
-    @State var buses = [Bus]()
     @State private var filteredBuses = [Bus]()
 
+    @State private var sourceStoppageEditing = false
+    @State private var destinationStoppageEditing = false
+    @State private var suggestionMenuYOffset = 0.0
+    @State private var startingStoppage: Suggestion?
+    @State private var destinationStoppage: Suggestion?
+
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading) {
-                HStack {
-                    VStack {
-                        Image(systemName: "figure.wave")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 20, height: 40)
-                            .padding(.trailing, 5)
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .top)) {
+            NavigationStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        VStack {
+                            Image(systemName: "figure.wave")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20, height: 40)
+                                .padding(.trailing, 5)
 
-                        Spacer()
+                            Spacer()
 
-                        Image(systemName: "mappin.and.ellipse")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 20, height: 40)
-                            .padding(.trailing, 5)
+                            Image(systemName: "mappin.and.ellipse")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20, height: 40)
+                                .padding(.trailing, 5)
+                        }
+
+                        VStack {
+                            // MARK: - Starting location textfield
+                            GeometryReader { reader in
+                                let frame = reader.frame(in: CoordinateSpace.global)
+                                TextField("Starting point", text: $startingLocation, onEditingChanged: { editing in
+                                    sourceStoppageEditing = editing
+                                })
+                                .padding()
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(.gray)
+                                }
+                                .onTapGesture {
+                                    suggestionMenuYOffset = frame.origin.y
+                                }
+                                .onChange(of: startingStoppage) {
+                                    startingLocation = startingStoppage?.title ?? startingLocation
+                                    filterBuses()
+                                }
+                            }
+
+                            Spacer()
+
+                            // MARK: - Ending location textfield
+                            GeometryReader { reader in
+                                let frame = reader.frame(in: CoordinateSpace.global)
+                                TextField("Ending point", text: $endingLocation, onEditingChanged: { editing in
+                                    destinationStoppageEditing = editing
+                                })
+                                .padding()
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(.gray)
+                                }
+                                .onTapGesture {
+                                    suggestionMenuYOffset = frame.origin.y
+                                }
+                                .onChange(of: destinationStoppage) {
+                                    endingLocation = destinationStoppage?.title ?? endingLocation
+                                    filterBuses()
+                                }
+                            }
+                        }
+                    }
+                    .frame(height: 120)
+                    .padding()
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.gray, lineWidth: 2)
                     }
 
-                    VStack {
-                        TextField("Starting point", text: $startingLocation)
-                            .padding()
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.gray)
-                            }
-                            .onChange(of: startingLocation) {
-                                filterBuses()
-                            }
+                    if filteredBuses.isEmpty {
+                        Text("No buses found.")
+                            .font(.title)
+                            .foregroundStyle(.teal)
+                            .padding(.top, 20)
+                    } else {
+                        Text("Search results")
+                            .font(.title2)
 
-                        Spacer()
-
-                        TextField("Ending point", text: $endingLocation)
-                            .padding()
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.gray)
+                        // MARK: - Search results
+                        List(filteredBuses, id: \.self) { bus in
+                            NavigationLink {
+                                BusDetailView(bus: bus)
+                                    .toolbar(.hidden, for: .tabBar)
+                            } label: {
+                                BusRow(bus: bus)
                             }
-                            .onChange(of: endingLocation) {
-                                filterBuses()
-                            }
+                        }
+                        .listStyle(.inset)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(.gray, lineWidth: 2)
+                        }
                     }
+
+                    Spacer()
                 }
-                .frame(height: 120)
                 .padding()
-                .overlay {
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(.gray, lineWidth: 2)
+            }
+
+            // MARK: - Suggestion menu
+            if showSuggestions() {
+                let suggestions = Binding<[Suggestion]>  {
+                    createSuggestions()
+                } set: { _, _ in
                 }
 
-                Text("Search results")
-                    .font(.title2)
-
-                List(filteredBuses, id: \.self) { bus in
-                    NavigationLink {
-                        BusDetailView(bus: bus)
-                            .toolbar(.hidden, for: .tabBar)
-                    } label: {
-                        BusRow(bus: bus)
+                if !suggestions.isEmpty {
+                    VStack {
+                        SuggestionMenuView(suggestions: suggestions, selected: sourceStoppageEditing ? $startingStoppage : $destinationStoppage)
+                            .offset(y: suggestionMenuYOffset)
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .offset(y: suggestionMenuYOffset)
+                            )
+                            .shadow(color: .gray, radius: 20)
+                            .padding(.horizontal, 30)
                     }
                 }
-                .listStyle(.inset)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(.gray, lineWidth: 2)
-                }
-
-                Spacer()
             }
-            .padding()
         }
-        .onAppear {
-            filteredBuses = buses
-        }
+    }
+
+    private func showSuggestions() -> Bool {
+        return sourceStoppageEditing || destinationStoppageEditing
+    }
+
+    private func createSuggestions() -> [Suggestion] {
+        let filterText = sourceStoppageEditing ? startingLocation : endingLocation
+
+        return firebaseData.stoppages
+            .filter { $0.value.name.caseInsensitiveContains(filterText) }
+            .map {
+                Suggestion(
+                    id: $0.key,
+                    title: $0.value.name,
+                    subTitle: ""
+                )
+            }
     }
 
     private func filterBuses() {
-        print(startingLocation, endingLocation)
-        filteredBuses = buses.filter { contains(stoppages: $0.stoppages) }
+        filteredBuses = firebaseData.buses.filter { contains(stoppages: $0.stoppages) }
     }
 
     private func contains(stoppages: [Stoppage]) -> Bool {
-        return stoppages.contains(where: { $0.name == startingLocation }) && stoppages.contains(where: { $0.name == endingLocation })
+        return stoppages.contains(where: { $0.name.caseInsensitiveEqual(startingLocation) }) && stoppages.contains(where: { $0.name.caseInsensitiveContains(endingLocation) })
     }
 }
 
 #Preview {
-    SearchView(buses: Constants.previewBuses)
+    SearchView()
         .environmentObject(FirebaseData())
 }
