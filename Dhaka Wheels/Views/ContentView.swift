c71e312c3af4ee781c0b22a6a5369bc510ae1405
Md. Asadul Islam <asadul_islam@outlook.com>
2023-12-07 21:59:57 +0600

    Handle swipe action on tab view
    Signed-off-by: Md. Asadul Islam <asadul_islam@outlook.com>

--- Dhaka Wheels/Views/ContentView.swift
+++ Dhaka Wheels/Views/ContentView.swift
@@ -9,23 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject private var firebaseData: FirebaseData
+    @State private var selectedTab = 0
 
     var body: some View {
-        TabView {
-            SearchView()
-                .tabItem {
-                    Label("Search", systemImage: "magnifyingglass")
-                }
-                .environmentObject(firebaseData)
+        TabView(selection: $selectedTab) {
+            Group {
+                SearchView()
+                    .tabItem {
+                        Label("Search", systemImage: "magnifyingglass")
+                    }
+                    .tag(0)
 
-            AllBustList()
-                .tabItem {
-                    Label("All Bus", systemImage: "list.bullet.circle.fill")
-                }
-                .environmentObject(firebaseData)
+                AllBustList()
+                    .tabItem {
+                        Label("All Bus", systemImage: "list.bullet.circle.fill")
+                    }
+                    .tag(1)
+            }
+            .environmentObject(firebaseData)
+            .highPriorityGesture(DragGesture().onEnded { handleSwipe(translation: $0.translation.width) })
         }
         .onAppear {
             UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance()
+        }
+    }
+
+    private func handleSwipe(translation: CGFloat) {
+        let minDragTranslation = 100.0
+
+        if translation > minDragTranslation {
+            selectedTab = max(0, selectedTab - 1)
+        } else if translation < -minDragTranslation {
+            selectedTab = min(selectedTab + 1, 1)
         }
     }
 }
