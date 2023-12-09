f5e88277adcc22af34bd4bad24b43f115bd13a1e
Md. Asadul Islam <asadul_islam@outlook.com>
2023-12-10 02:00:52 +0600

    Added navigation bar to the map view

--- Dhaka Wheels/Views/Bus/BusDetailView.swift
+++ Dhaka Wheels/Views/Bus/BusDetailView.swift
@@ -12,89 +12,107 @@ struct BusDetailView: View {
     @State private var showMap = false
 
     var body: some View {
-        VStack {
-            HStack {
-                VStack {
-                    AsyncImage(url: URL(string: bus.imageUrl)) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100)
-                    } placeholder: {
-                        Image(systemName: "bus.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
+        ZStack {
+            Color.lf6D01
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    VStack {
+                        AsyncImage(url: URL(string: bus.imageUrl)) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100)
+                        } placeholder: {
+                            Image(systemName: "bus.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                        }
                     }
+                    
+                    VStack(alignment: .leading) {
+                        Text("\(bus.stoppages.first?.name ?? "") - \(bus.stoppages.last?.name ?? "")")
+                        Text("Route No: \(bus.routeNumber)")
+                    }
+                    .font(.title2)
+                    .padding()
                 }
-
-                VStack(alignment: .leading) {
-                    Text("\(bus.stoppages.first?.name ?? "") - \(bus.stoppages.last?.name ?? "")")
-                    Text("Route No: \(bus.routeNumber)")
-                }
-                .font(.title2)
                 .padding()
-            }
-            .padding()
-            .overlay {
-                RoundedRectangle(cornerRadius: 20)
-                    .stroke(.gray, lineWidth: 2)
-            }
-
-            HStack {
-                Text("Routes")
-                    .font(.title)
-                    .padding(.top)
-                    .foregroundStyle(.blue)
-
-                Button {
-                    showMap = true
-                } label: {
-                    Image(systemName: "map.fill")
-                    Text("View in Map")
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.lffD1E)
+                )
+                .overlay {
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(.gray, lineWidth: 2)
                 }
-                .font(.title2)
-                .padding(5)
-                .foregroundStyle(.white)
-                .background(.blue)
-                .cornerRadius(10)
-                .padding(.top)
-            }
-
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack {
-                    ForEach(Array(bus.stoppages.enumerated()), id: \.element) { i, stoppage in
-                        VStack {
-                            if i == 0 {
-                                Image(systemName: "circle.circle")
-                                    .padding(5)
-                                    .foregroundStyle(.blue, .gray)
-                            } else {
-                                Image(systemName: "arrow.up.arrow.down")
-                                    .padding(5)
-                                    .foregroundStyle(.red, .green)
-                            }
-
-                            Text(stoppage.name)
-
-                            if i == bus.stoppages.count - 1 {
-                                Image(systemName: "mappin.and.ellipse")
-                                    .padding(5)
-                                    .foregroundStyle(.red)
+                
+                HStack {
+                    Spacer()
+                    Text("Routes")
+                        .font(.title)
+                        .padding(.top)
+                        .foregroundStyle(.blue)
+                    
+                    Spacer()
+                    
+                    NavigationLink {
+                        MapView(stoppages: bus.stoppages)
+                            .navigationTitle("Routes")
+                    } label: {
+                        HStack {
+                            Image(systemName: "map.fill")
+                            
+                            Text("View on Map")
+                        }
+                        .font(.title2)
+                        .padding(5)
+                        .foregroundStyle(.white)
+                        .background(.blue)
+                        .cornerRadius(10)
+                        .padding(.top)
+                    }
+                    
+                    Spacer()
+                }
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack {
+                        ForEach(Array(bus.stoppages.enumerated()), id: \.element) { i, stoppage in
+                            VStack {
+                                if i == 0 {
+                                    Image(systemName: "circle.circle")
+                                        .padding(5)
+                                        .foregroundStyle(.blue, .gray)
+                                } else {
+                                    Image(systemName: "arrow.up.arrow.down")
+                                        .padding(5)
+                                        .foregroundStyle(.red, .green)
+                                }
+                                
+                                Text(stoppage.name)
+                                
+                                if i == bus.stoppages.count - 1 {
+                                    Image(systemName: "mappin.and.ellipse")
+                                        .padding(5)
+                                        .foregroundStyle(.red)
+                                }
                             }
                         }
                     }
+                    .padding()
+                    .frame(maxWidth: .infinity)
                 }
-                .padding()
-                .frame(maxWidth: .infinity)
             }
-        }
-        .fullScreenCover(isPresented: $showMap) {
-            MapView(stoppages: bus.stoppages)
+            .padding()
         }
     }
 }
 
 #Preview {
-    BusDetailView(bus: Constants.previewBuses[0])
+    NavigationStack {
+        BusDetailView(bus: Constants.previewBuses[0])
+    }
 }
--- Dhaka Wheels/Views/Stoppage/MapView.swift
+++ Dhaka Wheels/Views/Stoppage/MapView.swift
@@ -9,47 +9,24 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @Environment(\.dismiss) private var dismiss
-
     @State var stoppages = [Stoppage]()
     @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
     @State private var selectedStoopage: MKMapItem?
 
     var body: some View {
-        ZStack {
-
-            Map(position: $position, selection: $selectedStoopage) {
-                ForEach(stoppages) { stoppage in
-                    Marker(stoppage.name, image: "bus_station", coordinate: CLLocationCoordinate2D(latitude: stoppage.latitude, longitude: stoppage.longitude))
-                        .tint(.blue)
-                }
-            }
-            .mapStyle(.hybrid(elevation: .flat))
-            .mapControls {
-                MapCompass()
-                MapScaleView()
-            }
-
-            VStack(alignment: .center) {
-                HStack {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Text("< Back")
-                            .padding(8)
-                            .foregroundStyle(.black)
-                            .background(.white)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                    }
-                    .padding()
-
-                    Spacer()
-                }
-
-                Spacer()
+        Map(position: $position, selection: $selectedStoopage) {
+            ForEach(stoppages) { stoppage in
+                Marker(stoppage.name, image: "bus_station", coordinate: CLLocationCoordinate2D(latitude: stoppage.latitude, longitude: stoppage.longitude))
+                    .tint(.blue)
             }
         }
+        .mapStyle(.hybrid(elevation: .flat))
+        .mapControls {
+            MapCompass()
+            MapScaleView()
+        }
     }
+
 }
 
 #Preview {
